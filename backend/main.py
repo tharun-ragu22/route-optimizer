258c87cfa6f6cf4ecb91dc3f9c88f696dc7d3c66
Tharun Ragulan <t2ragula@uwaterloo.ca>
2026-02-25 13:17:11 -0500

    test using api as interface for agent

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/get_best_time")
+def get_best_time(
+    src: str,
+    dst: str,
+    time: str
+):
+    pass
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
