04757c88498cb9c46637450ab616cf737276cfd5
Tharun Ragulan <t2ragula@uwaterloo.ca>
2026-02-28 16:19:50 -0500

    red test for bad date selection

--- frontend/__tests__/routingform.test.js
+++ frontend/__tests__/routingform.test.js
@@ -77,4 +77,43 @@ describe('RoutingForm Component', () => {
         // Assert that the mock was called once
         expect(mockSubmit).toHaveBeenCalledTimes(1);
     });
+
+    it('does not submit when time range is not correct', async () => {
+        // Given the user has filled out source and destination
+        const user = userEvent.setup();
+        const mockSubmit = jest.fn();
+        render(<RoutingForm onSubmit={mockSubmit}/>);
+        const sourceContainer= await screen.getByTestId('source-wrapper')
+        const sourceInput = await screen.findByPlaceholderText("Source Address");
+        
+        await user.type(sourceInput, '300 Kingston Rd');
+        const suggestion = await within(sourceContainer).findByText(/300/i);
+        await user.click(suggestion);
+
+        const destinationContainer= await screen.getByTestId('destination-wrapper')
+        const destinationInput = await screen.findByPlaceholderText("Destination Address");
+        
+        
+        await user.type(destinationInput, '750 Kingston Rd');
+        const destinationSuggestion = await within(destinationContainer).findByText(/750/i);
+        await user.click(destinationSuggestion);
+
+        // And source to destination is driveable
+        // And user selects time range
+
+        const leaveTimeMin = await screen.getByTestId('leave-time-min')
+        await user.type(leaveTimeMin, '17:30')
+        const leaveTimeMax = await screen.getByTestId('leave-time-max')
+        await user.type(leaveTimeMax, '17:00')
+
+        // and time range is correct
+        // When user hits submit
+
+        const button = screen.getByRole('button', { name: "Submit" });
+        await user.click(button);
+        // Then they get the minimum time
+        
+        // Assert that the mock was called once
+        expect(mockSubmit).toHaveBeenCalledTimes(0);
+    });
 });
