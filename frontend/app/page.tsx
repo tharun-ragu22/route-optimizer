b11a1b893c074b039b12a19abbcf48f36d82ecb3
Tharun Ragulan <t2ragula@uwaterloo.ca>
2026-03-05 11:41:37 -0500

    make page purpose clearer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -35,6 +35,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1>Route Optimizer</h1>
+
+        <h3>Find the best time to leave to avoid traffic.</h3>
         
         <div className="p-10">
           <LoadScript
